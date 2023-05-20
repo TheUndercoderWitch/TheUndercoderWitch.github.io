0f987430e9ed9b4af52bd89b2cdcf4f1a0c62b1d
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2023-05-19 22:30:13 -0400

    push12

--- home.js
+++ home.js
@@ -1,28 +1,19 @@
-/*--- banner variables - start ---*/
-const bannerPicture = document.getElementById("banner_picture");
-/*--- banner variables - end ---*/
-
-/*--- mission variables - start */
-const missionPicture = document.getElementById("mission_img");
-/*--- mission variables - end */
-
-/*--- contact variables - start ---*/
-const contactPicture = document.getElementById("contact_img");
-/*--- contact variables - end ---*/
-
 /*--- banner code - start ---*/
+const bannerPicture = document.getElementById("banner_picture");
 window.addEventListener("scroll",()=>{
     bannerPicture.style.top = window.scrollY/2 + "px";
 })
 /*--- banner code - end ---*/
 
 /*--- mission code - start */
+const missionPicture = document.getElementById("mission_img");
 window.addEventListener("scroll",()=>{
     missionPicture.style.top = window.scrollY/4 + "px";
 })
 /*--- mission code - end */
 
 /*--- contact code - start ---*/
+const contactPicture = document.getElementById("contact_img");
 window.addEventListener("scroll",()=>{
     contactPicture.style.top = window.scrollY/4 + "px";
 })
