646376723cdbc06777aa17e80b4b9dd77cf3772c
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2023-05-20 12:14:33 -0400

    push15

--- home.js
+++ home.js
@@ -1,20 +1,10 @@
-/*--- banner code - start ---*/
+/*--- parallax - start ---*/
 const bannerPicture = document.getElementById("banner_picture");
-window.addEventListener("scroll",()=>{
-    bannerPicture.style.top = window.scrollY/2 + "px";
-})
-/*--- banner code - end ---*/
-
-/*--- mission code - start */
 const missionPicture = document.getElementById("mission_img");
-window.addEventListener("scroll",()=>{
-    missionPicture.style.top = window.scrollY/4 + "px";
-})
-/*--- mission code - end */
-
-/*--- contact code - start ---*/
 const contactPicture = document.getElementById("contact_img");
 window.addEventListener("scroll",()=>{
-    contactPicture.style.top = window.scrollY/4 + "px";
+    bannerPicture.style.top = window.scrollY/2 + "px";
+    missionPicture.style.top = window.scrollY/5 + "px";
+    contactPicture.style.top = window.scrollY/5 + "px";
 })
-/*--- contact code - end ---*/
+/*--- parallax - start ---*/
